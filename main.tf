b0874020101ee0831b6d7e2ccf99f7141fc39b5d
Yeji Kim <yejif15@gmail.com>
2024-03-04 16:49:06 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
-resource "aws_elasticache_replication_group" "bad_example" {
+# resource "aws_elasticache_replication_group" "bad_example" {
+#          replication_group_id = "foo"
+#          replication_group_description = "my foo cluster"
+#          transit_encryption_enabled = true
+#          at_rest_encryption_enabled = true
+#  } 
+
+resource "aws_elasticache_replication_group" "good_example" {
          replication_group_id = "foo"
          replication_group_description = "my foo cluster"
-         transit_encryption_enabled = true
+
          at_rest_encryption_enabled = true
- } 
+         transit_encryption_enabled = true
+
+ }
+
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
 }
