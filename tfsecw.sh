84ea0938bafe42c5d0eb901c71cca92757bc13d3
Yeji Kim <yejif15@gmail.com>
2024-03-04 17:25:43 +0900

    Update tfsecw.sh

--- tfsecw.sh
+++ tfsecw.sh
@@ -10,7 +10,7 @@ echo "$(pwd)"
 
 # docker run --rm -v "$(pwd):/src" aquasec/tfsec /src --no-color -f junit > tfsec_result.xml
 # docker run --rm -v "$(pwd):/src" aquasec/tfsec /src -f junit > tfsec_results.xml
-# docker run --rm -v $(pwd)/:/src aquasec/tfsec /src --no-color
+docker run --rm -v $(pwd)/:/src aquasec/tfsec /src --no-color
 
 curl -fsSLO https://github.com/aquasecurity/tfsec/releases/download/v1.18.0/tfsec_1.18.0_linux_arm64.tar.gz
 
