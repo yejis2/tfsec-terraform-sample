b9597776b494937323fdcb65368539f951976472
Yeji Kim <yejif15@gmail.com>
2024-03-04 17:19:54 +0900

    Update tfsecw.sh

--- tfsecw.sh
+++ tfsecw.sh
@@ -18,9 +18,9 @@ mkdir "$(pwd)/tfsec_dir"
 
 tar xzvf tfsec_1.18.0_linux_arm64.tar.gz -C "$(pwd)/tfsec_dir"
 
-# ls -alt "$(pwd)/tfsec_dir"
+ls -alt "$(pwd)/tfsec_dir"
 
-# $(pwd)/tfsec_dir/tfsec . -f junit > tfsec_results.xml
+$(pwd)/tfsec_dir/tfsec . -f junit > tfsec_results.xml
 
 # echo "------------------------------------------------"
 # cat tfsec_results.xml
