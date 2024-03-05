57152cb78e0d81ff7f43fd717ac65e058f890a5d
Yeji Kim <yejif15@gmail.com>
2024-03-05 15:18:57 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 resource "aws_elasticache_replication_group" "pass_example" {
-         replication_group_id = "foo"
-         replication_group_description = "my foo cluster"
-         transit_encryption_enabled = true
-         at_rest_encryption_enabled = true
-         # transit_encryption_enabled = false
-         # at_rest_encryption_enabled = false
-} 
+  replication_group_id           = "foo"
+  transit_encryption_enabled     = true
+  at_rest_encryption_enabled     = true
+}
 
 output "jenkins_terraform" {
   value = "running Terraform from Jenkins"
