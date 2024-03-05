b309f7cb3a15833f0859b4723aac153a71309007
Yeji Kim <yejif15@gmail.com>
2024-03-05 15:36:19 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,9 @@
-# resource "aws_elasticache_replication_group" "pass_example" {
-#   replication_group_id           = "foo"
-#   transit_encryption_enabled     = true
-#   at_rest_encryption_enabled     = true
-# }
-
-# output "jenkins_terraform" {
-#   value = "running Terraform from Jenkins"
-# }
-
-# 잘못된 VPC CIDR 블록
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/8" # 잘못된 CIDR 블록 형식
+resource "aws_elasticache_replication_group" "pass_example" {
+  replication_group_id           = "foo"
+  transit_encryption_enabled     = true
+  at_rest_encryption_enabled     = true
 }
 
-
+output "jenkins_terraform" {
+  value = "running Terraform from Jenkins"
+}
