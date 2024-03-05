62616b0b39063bee69ac54aef4f69e24e068bde6
Yeji Kim <yejif15@gmail.com>
2024-03-05 15:53:49 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
-resource "aws_elasticache_replication_group" "pass_example" {
-  replication_group_id           = "foo"
-  transit_encryption_enabled     = true
-  at_rest_encryption_enabled     = true
-}
+# resource "aws_elasticache_replication_group" "pass_example" {
+#   replication_group_id           = "foo"
+#   transit_encryption_enabled     = true
+#   at_rest_encryption_enabled     = true
+# }
 
-output "jenkins_terraform" {
-  value = "running Terraform from Jenkins"
+# output "jenkins_terraform" {
+#   value = "running Terraform from Jenkins"
+# }
+
+# VPC test
+resource "aws_vpc" "vpc-test-jenkins" {
+  cidr_block  = "10.0.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "vpc-test-jenkins"
+  }
 }
