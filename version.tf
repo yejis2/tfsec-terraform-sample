c363cc0a88ff36cd217a75dbd92d69de9d41732e
Yeji Kim <yejif15@gmail.com>
2024-03-05 10:09:21 +0900

    Update version.tf

--- version.tf
+++ version.tf
@@ -1,3 +1,20 @@
-terraform {
-  required_version = ">=1.0.0"
+provider "aws" {
+  region = "ap-northeast-2"
 }
+
+terraform {
+  required_version = ">= 1.0.0"
+
+  backend "s3" {
+       # Replace this with your bucket name!
+      bucket = "terraform-remote-state-testbkt"
+      key = "global/s3/terraform.tfstate"
+      region= "ap-northeast-2"
+      # Replace this with your DynamoDB table name!
+      dynamodb_table = "terraform-remote-state-dynamo"
+      encrypt        = true
+     }
+}
+
+#### Remote State Data Source ####
+data "aws_availability_zones" "all" {}
